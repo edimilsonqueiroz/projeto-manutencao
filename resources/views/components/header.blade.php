31635ed22fd66eecbf1967e30ff6bfac59eb17ed
Edimilson Queiroz <edimilsonqueiroz681@gmail.com>
2023-09-02 12:02:53 -0300

    pronto

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,18 +4,21 @@
             Sidebar
             <button class="md:hidden" x-on:click="openSidebar = true">Fechar Sidebar</button>
         </div>
-        <div class="flex-1 h-full flex flex-col md:ml-64">
+        <div x-data="{openLogout: false}" class="flex-1 h-full flex flex-col md:ml-64">
             <header class="bg-gray-50 h-16 md:px-3 px-2 shadow flex items-center justify-between">
                 <button x-on:click="openSidebar = false">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5" />
                     </svg>
                 </button>
-                <button class="flex">
+                <button x-on:click="openLogout = !openLogout" class="flex">
                     <span class="mr-1">Edimilson Francisco de Queiroz</span>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                     </svg>
                 </button>
             </header>
+            <div x-show="openLogout" class="fixed top-12 right-16 w-28 h-20 z-20 bg-slate-400">
+                Modal Logout
+            </div>
             <div class="bg-gray-300 w-full p-3 py-5  flex flex-1 overflow-y-auto">
