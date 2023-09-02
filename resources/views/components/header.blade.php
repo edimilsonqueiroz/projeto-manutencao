d6efbb2100351254f1cadda562d8b3b3ae17b521
Edimilson Queiroz <edimilsonqueiroz681@gmail.com>
2023-09-02 10:52:02 -0300

    pronto

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full h-full">
     <div x-data="{openSidebar: true}" class="w-full flex h-full">
-        <div  :class="{'translate-x-0': !openSidebar}" class="w-64 bg-slate-600 h-full transform transition ease-in-out fixed md:fixed z-30 duration-200 -translate-x-full md:-translate-x-0">
+        <div x-transition  :class="{'translate-x-0': !openSidebar}" class="w-64 bg-slate-600 h-full transform transition ease-in-out fixed md:fixed z-30 duration-200 -translate-x-full md:-translate-x-0">
             Sidebar
             <button class="md:hidden" x-on:click="openSidebar = true">Fechar Sidebar</button>
         </div>
