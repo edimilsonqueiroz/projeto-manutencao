c48ed93f6d59207d8c701b1d5ff17b78252b403d
Edimilson Queiroz <edimilsonqueiroz681@gmail.com>
2023-09-02 11:51:31 -0300

    pronto

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,11 +1,21 @@
 <div class="w-full h-full">
     <div x-data="{openSidebar: true}" class="w-full flex h-full">
-        <div x-transition  :class="{'translate-x-0': !openSidebar}" class="w-64 bg-slate-600 h-full transform transition ease-in-out fixed md:fixed z-30 duration-200 -translate-x-full md:-translate-x-0">
+        <div x-transition  :class="{'translate-x-0': !openSidebar}" class="w-64 bg-slate-600 h-full transform transition ease-in-out fixed left-0 top-0 bottom-0 max-h-full md:fixed z-30 duration-200 -translate-x-full md:-translate-x-0">
             Sidebar
             <button class="md:hidden" x-on:click="openSidebar = true">Fechar Sidebar</button>
         </div>
         <div class="flex-1 h-full flex flex-col md:ml-64">
-            <header>
-                <button x-on:click="openSidebar = false">Abrir Sidebar</button>
+            <header class="bg-gray-50 h-16 md:px-3 px-2 shadow flex items-center justify-between">
+                <button x-on:click="openSidebar = false">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5" />
+                    </svg>
+                </button>
+                <button class="flex">
+                    <span class="mr-1">Edimilson Francisco de Queiroz</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                    </svg>
+                </button>
             </header>
-            <div>
+            <div class="bg-gray-300 w-full p-3 py-5  flex flex-1 overflow-y-auto">
